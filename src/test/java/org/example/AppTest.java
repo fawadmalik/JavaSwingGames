4b9e37a64a177625b92dcaae394086af6ea7c19e
Fawad Malik <fawad.malik@hotmail.com>
2024-06-29 23:42:14 -0700

    Adds sample swing game

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple BasketballGame.
+ */
+@Test
+public class AppTest
+{
+    public void verifyThis(){
+        Assert.assertTrue(true);
+    }
+}
